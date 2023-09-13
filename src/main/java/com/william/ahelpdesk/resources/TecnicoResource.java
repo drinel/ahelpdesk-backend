8c40fdd1ce8e1d28c01f270e28e4e7af547ebe42
drinel <will.i.am.002@hotmail.com>
2023-09-13 12:29:58 -0300

    CrossOrigin

--- src/main/java/com/william/ahelpdesk/resources/TecnicoResource.java
+++ src/main/java/com/william/ahelpdesk/resources/TecnicoResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/tecnicos")
+@CrossOrigin("*")
 public class TecnicoResource {
 	
 	@Autowired
